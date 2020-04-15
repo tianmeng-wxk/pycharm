ffc806df3618890e88ae723d4c8245ba707490cb
Administrator <3394788013@qq.com>
2020-04-15 09:15:49 +0800

    04.15

--- testingshop/test_case/test_cases.py
+++ testingshop/test_case/test_cases.py
@@ -3,7 +3,8 @@ import sys
 curPath = os.path.abspath(os.path.dirname(__file__))
 rootPath = os.path.split(curPath)[0]
 sys.path.append(rootPath)
-sys.path.append('../../')
+sys.path.append('E:\\pycharm')
+print(sys.path)
 import unittest
 from selenium import webdriver
 from time import sleep
