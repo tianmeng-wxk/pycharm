a343185df5bec8209b68f95a49607fb2555033f4
Administrator <3394788013@qq.com>
2020-06-12 19:48:43 +0800

    0612

--- 滑动验证码识别/20200609/01web自动化滑动验证码破解.py
+++ 滑动验证码识别/20200609/01web自动化滑动验证码破解.py
@@ -1,11 +1,3 @@
-"""
-============================
-Author:柠檬班-木森
-Time:2020/6/9   14:45
-E-mail:3247119728@qq.com
-Company:湖南零檬信息技术有限公司
-============================
-"""
 from selenium import webdriver
 from slideVerfication import SlideVerificationCode
 
